bcdd2b9dd79072ee34efe2b7f1681215a0f3aed8
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-07-01 10:03:51 -0300

    Tela de detalhamento do país selecionado

--- app_ibge_data_consultation/lib/screens/country.dart
+++ app_ibge_data_consultation/lib/screens/country.dart
@@ -1,15 +1,26 @@
+import 'package:app_ibge_data_consultation/models/countryList.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CountryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Detalhamento de País'),
-      ),
-      body: Column(
-        children: [Text('Tela de detalhamento do pais')],
-      ),
-    );
+    Map<String, int> args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, int>;
+    return Consumer<CountryList>(builder: (context, countryList, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Detalhamento de País'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              'Brasil',
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      );
+    });
   }
 }
