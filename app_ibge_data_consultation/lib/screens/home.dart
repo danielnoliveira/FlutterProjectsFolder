aa112ba4d9a426261baf9edbb6ec63f83bb2d7c2
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-07-01 10:03:31 -0300

    Tela inicial

--- app_ibge_data_consultation/lib/screens/home.dart
+++ app_ibge_data_consultation/lib/screens/home.dart
@@ -1,15 +1,34 @@
+import 'package:app_ibge_data_consultation/models/countryList.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
+  TextStyle buttonStyle = TextStyle();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Column(
-        children: [Text('Tela Inicial')],
-      ),
+    return Consumer<CountryList>(
+      builder: (context, countryList, child) {
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+            body: ListView.builder(
+                itemCount: countryList.countrys.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    color: Colors.blue,
+                    margin: EdgeInsets.symmetric(vertical: 5.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/country',
+                              arguments: {'index': index});
+                        },
+                        child: Text(
+                            '${countryList.countrys[index]["nome"]["abreviado"]}')),
+                  );
+                },
+                padding: EdgeInsets.all(20)));
+      },
     );
   }
 }
