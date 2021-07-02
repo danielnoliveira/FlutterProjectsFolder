dfbd8c0b322bc39ad1c085ee60e00b2fa473ce3b
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-07-02 00:12:49 -0300

    Funções de filtragem e excluir

--- app_ibge_data_consultation/lib/models/countryList.dart
+++ app_ibge_data_consultation/lib/models/countryList.dart
@@ -6,7 +6,9 @@ import 'package:http/http.dart' as http;
 class CountryList extends ChangeNotifier {
   List<dynamic> _countrys = [];
 
-  List<dynamic> get countrys => _countrys;
+  List<dynamic> _mutableList = [];
+
+  List<dynamic> get countrys => _mutableList;
 
   CountryList() {
     populateList();
@@ -15,11 +17,36 @@ class CountryList extends ChangeNotifier {
   void populateList() async {
     var url = Uri.parse('https://servicodados.ibge.gov.br/api/v1/paises/');
     var response = await http
-        .get(url, headers: {'Content-Type': 'application/json; charset=UTF-8'});
+        .get(url, headers: {'Content-Type': 'application/json; charset=utf-8'});
 
     if (response.statusCode == 200) {
-      countrys.addAll(jsonDecode(response.body) as List);
+      var result = json.decode(utf8.decode(response.bodyBytes));
+      _countrys.addAll(result);
+      _mutableList.addAll(result);
     }
+    notifyListeners();
+  }
+
+  void filterList(String text) {
+    List<dynamic> novaLista = _countrys
+        .where((element) => (element["nome"]["abreviado"] as String)
+            .toLowerCase()
+            .contains(text))
+        .toList();
+    _mutableList = novaLista;
+    notifyListeners();
+  }
+
+  void deleteCountry(index) {
+    var excludedCountryM49 = _mutableList[index]['id']['M49'];
+    var novaLista = _countrys
+        .where((element) => element['id']['M49'] != excludedCountryM49)
+        .toList();
+    var novaLista2 = _mutableList
+        .where((element) => element['id']['M49'] != excludedCountryM49)
+        .toList();
+    _countrys = novaLista;
+    _mutableList = novaLista2;
     notifyListeners();
   }
 }
